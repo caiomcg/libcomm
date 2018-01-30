9f29aed5faa5eb7f842537ae0e27d95e71514190
caiomcg <caiomcg@gmail.com>
2018-01-30 14:49:33 -0300

    Add test to main

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,22 @@
 auto main() -> int {
     DatagramSocket datagram_socket{Uri{"udp://127.0.0.1:8080"}};
     datagram_socket.bind();
-    
+
+    auto buffer = std::make_shared<uint8_t>(1024);
+    buffer.get()[0] = 'T';
+    buffer.get()[1] = 'E';
+    buffer.get()[2] = 'S';
+    buffer.get()[3] = 'T';
+    buffer.get()[4] = '\0';
+
+    std::clog << "Listening at: " << datagram_socket << std::endl;
+
+    auto packet = datagram_socket.receive(1024);
+
+    std::clog << "Message: " << packet.getData().get() << " - Size: " << packet.getSize() << std::endl;
+
+    std::clog << "Sending to: " << packet.getClient() << " - message: " << buffer.get() << std::endl;
+
+    datagram_socket.send(packet.getClient(), buffer, 5);
     return 0;
 }
